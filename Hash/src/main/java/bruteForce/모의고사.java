195f61123b79d5f1a30ebf2c37339b2fe1e3ebd3
solbae <sol4854@naver.com>
2022-10-31 10:06:26 +0900

    최댓값 찾아 학생 정답 비교하기.  풀이중 =  중복자 있는경우 ,출력

--- Hash/src/main/java/bruteForce/모의고사.java
+++ Hash/src/main/java/bruteForce/모의고사.java
@@ -9,16 +9,30 @@ public class 모의고사 {
 
         int[] cnt = new int[3];
         for (int i = 0; i < answers.length; i++) {
-            if(student1[i % student1.length] == answers[i]){
-                cnt[0]++;
-            }
+            if(student1[i % student1.length] == answers[i]) cnt[0]++;
+            if(student2[i % student2.length] == answers[i]) cnt[1]++;
+            if(student3[i % student3.length] == answers[i]) cnt[2]++;
         }
-        System.out.println(cnt[0]);
+
+        //많이 맞춘 점수찾기
+        int max = cnt[0]; //최댓값 구하기
+        for (int i = 0; i < cnt.length; i++) {
+            if(max < cnt[i])
+                max = cnt[i];
+        }
+        //max와 학생들 점수 비교
+        StringBuilder result = new StringBuilder();
+        if(cnt[0] == max) result.append(1);
+        if(cnt[1] == max) result.append(2);
+        if(cnt[2] == max) result.append(3);
+
+        //중복일 경우 [,] 넣기...
+        System.out.println(result.toString());
         return cnt;
     }
 
     public static void main(String[] args) {
-        int[] answers = {1, 2, 3, 4, 5, 0, 0, 0, 0 ,5};
+        int[] answers = {1,3,2,4,2};
         solution(answers);
     }
 }
