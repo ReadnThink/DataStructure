7e863032e9c1c160c802e597a44b54316ccea89d
solbae <sol4854@naver.com>
2022-11-01 10:49:51 +0900

    소수찾기 TemplateCallback적용하여 3가지 방법으로 풀이

--- Hash/src/main/java/bruteForce/TemplateCallBackPrime.java
+++ Hash/src/main/java/bruteForce/TemplateCallBackPrime.java
@@ -3,32 +3,35 @@ package bruteForce;
 import javax.sound.sampled.EnumControl;
 import java.util.Enumeration;
 
+interface StatementStrategy{
+    boolean compare(int a, int b);
+};
 public class TemplateCallBackPrime {
     // i < num
     // i < num / 2
     // i * i < num
 
-    boolean someOperation(int a, int b){
-        return a < b;
-    }
-
-    boolean someOperation1(int a, int b){
-        return a < b / 2;
-    }
-
-    boolean someOperation2(int a, int b){
-        return a * a < b;
-    }
-
-    boolean isPrime(int n){
-        for (int i = 2; someOperation(i, n); i++) {
+    boolean isPrime(int n, StatementStrategy stmt){
+        for (int i = 2; stmt.compare(i, n); i++) {
             if(n % i == 0) return false;
         }
         return true;
     }
     public static void main(String[] args) {
         TemplateCallBackPrime templateCallBackPrime = new TemplateCallBackPrime();
-        boolean result = templateCallBackPrime.isPrime(12);
+        boolean result = templateCallBackPrime.isPrime(13, (a,b)-> a < b);
+        boolean result1 = templateCallBackPrime.isPrime(17, (a,b)-> a < b / 2);
+        boolean result2 = templateCallBackPrime.isPrime(15, (a,b)-> a * a < b);
+        boolean result3 = templateCallBackPrime.isPrime(12, new StatementStrategy() {
+            @Override
+            public boolean compare(int a, int b) {
+                return a * a <= b;
+            }
+        });
+
         System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
     }
 }
