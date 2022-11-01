8af2fa7f10eedf3701b521fa98565186f2a8d558
solbae <sol4854@naver.com>
2022-11-01 09:27:22 +0900

    소수찾기 특정숫자 한개가 소수인지 판별

--- Hash/src/main/java/bruteForce/소수찾기.java
+++ Hash/src/main/java/bruteForce/소수찾기.java
@@ -1,23 +1,22 @@
 package bruteForce;
 
 public class 소수찾기 {
-    public static int solution (int n) {
+    public static boolean isPrime (int n) {
         int num = n;
-        int answer = 0;
         // 2, 3, 5, 7, 9, 11, 13, 15, 17
         for (int i = 2; i <= num / 2; i++) {
-            if(i / 2 != 0){
-                answer++;
-                System.out.println("소수입니다.");
-                break;
+            if(i % n == 0){
+                System.out.println("소수가 아닙니다.");
+                return true;
             }
         }
-        return answer;
+        System.out.println("소수 입니다.");
+        return false;
     }
 
 
     public static void main(String[] args) {
         int n = 13;
-        System.out.println(solution(n));
+        System.out.println(isPrime(n));
     }
 }
