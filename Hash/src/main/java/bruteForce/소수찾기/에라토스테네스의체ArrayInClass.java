858bf12ecbd8337f1e40a04bbec9e8232fe684ce
solbae <sol4854@naver.com>
2022-11-03 10:13:37 +0900

    2를 제외한 2의배수를 제외한 수 출력

--- Hash/src/main/java/bruteForce/소수찾기/에라토스테네스의체ArrayInClass.java
+++ Hash/src/main/java/bruteForce/소수찾기/에라토스테네스의체ArrayInClass.java
@@ -1,11 +1,9 @@
 package bruteForce.소수찾기;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-public class 에라토스테네스의체Array {
+public class 에라토스테네스의체ArrayInClass {
     // 소수를 구할 때 2부터 N의 제곱근까지만 확인을 하면 된다는 이론
 
     //에네토스테네스 체 속도개선의 핵심
@@ -14,28 +12,29 @@ public class 에라토스테네스의체Array {
     public void solution(int n){
         int[] nums = new int[n]; // 2부터 값을 넣을 것이기 때문에 n보다 1큰숫자 까지 넣는다. n -1까지만 한다.
         boolean[] check = new boolean[n];
-//        Arrays.fill(check,true); // check를 true로 초기화
+        Arrays.fill(check,true); // check를 true로 초기화
 
         for (int i = 0; i < n; i++) {
             nums[i] = i + 2;
         }
 
-        for (int j = 2; j * j <= n ; j++) { // 에라토스테네스의체 2 ~ n의 제곱근까지 반복.
-            for (int i = 0; i < nums.length; i++) {
-                if(nums[i] % j == 0 && nums[i] > j) check[i] = false;
-//                else check[i] = true; //else에 걸리면 이상해진다...?
-            }
+        // 2의 배수 지우기
+        // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+        // 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 ...50
+        int multipleOf = 2;
+        for (int i = 2; i < nums.length; i+=2) {
+            check[i] = false;
         }
+
         for (int i = 0; i < check.length; i++) {
             if(check[i] == true){
                 System.out.println(nums[i]);
             }
-
         }
     }
 
     public static void main(String[] args) {
-        에라토스테네스의체Array s = new 에라토스테네스의체Array();
+        에라토스테네스의체ArrayInClass s = new 에라토스테네스의체ArrayInClass();
         System.out.println(LocalDateTime.now());
         s.solution(100);
         System.out.println(LocalDateTime.now());
