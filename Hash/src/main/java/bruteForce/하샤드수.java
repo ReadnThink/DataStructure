2360fa422a822fcc05b98940264c646392a249c9
solbae <sol4854@naver.com>
2022-11-07 09:19:26 +0900

    하샤드 수 풀이증

---
+++ Hash/src/main/java/bruteForce/하샤드수.java
@@ -0,0 +1,21 @@
+package bruteForce;
+
+public class 하샤드수 {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int num = x;
+        int sum = 0;
+        while(num > 0){
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println(sum);
+
+        return sum % x == 0;
+    }
+
+    public static void main(String[] args) {
+        하샤드수 h = new 하샤드수();
+        System.out.println(h.solution(10));
+    }
+}
