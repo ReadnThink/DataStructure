974b4231138cbb8440b960aa48e083a11f2fafc5
solbae <sol4854@naver.com>
2022-11-08 09:25:11 +0900

    code up 형식에 맞게 변경

--- DataStructureWithAlgorithm/src/main/java/bruteForce/데이터탐색.java
+++ DataStructureWithAlgorithm/src/main/java/bruteForce/데이터탐색.java
@@ -3,15 +3,18 @@ package bruteForce;
 import java.util.Scanner;
 
 public class 데이터탐색 {
-    public static void solution(int[] nums, int n){
+    public static int solution(int[] nums, int n){
         int idx = 1;
+        boolean check = false;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == n){
+                check = true;
                 break;
             }
             idx++;
         }
-        System.out.println(idx);
+        if(check == false) return -1;
+        return idx;
     }
 
     public static void main(String[] args) {
@@ -24,6 +27,6 @@ public class 데이터탐색 {
             nums[i] = sc.nextInt();
         }
         int N = sc.nextInt();
-        solution(nums,N);
+        System.out.println(solution(nums,N));
     }
 }
