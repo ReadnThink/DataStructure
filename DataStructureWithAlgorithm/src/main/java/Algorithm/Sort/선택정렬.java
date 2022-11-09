1d8a2fd6ba843c35eee8b4240d2b5a9e15c87410
solbae <sol4854@naver.com>
2022-11-09 10:05:00 +0900

    선택정렬 오름차순, 내림차순

--- DataStructureWithAlgorithm/src/main/java/Algorithm/Sort/선택정렬.java
+++ DataStructureWithAlgorithm/src/main/java/Algorithm/Sort/선택정렬.java
@@ -9,7 +9,7 @@ public class 선택정렬 {
         nums[i] = nums[j];
         nums[j] = tmp;
     }
-    public static void Selections(int[] nums){
+    public static void SelectionsUp(int[] nums){
 
         for(int i = 0; i < nums.length; i++){
             int min = nums[i];
@@ -24,12 +24,29 @@ public class 선택정렬 {
                 swap(nums,i,index);
             }
         }
-        for (int num : nums) {
-            System.out.printf("%d ,",num);
+        System.out.println(Arrays.toString(nums));
+    }
+    public static void SelectionsDown(int[] nums){
+
+        for(int i = 0; i < nums.length; i++){
+            int Max = nums[i];
+            int index = 0;
+            for (int j = i + 1; j < nums.length; j++) {
+                if(nums[j] > Max){
+                    Max = nums[j];
+                    index = j;
+                }
+            }
+            if(index != 0) {
+                swap(nums,i,index);
+            }
         }
+        System.out.println(Arrays.toString(nums));
     }
     public static void main(String[] args) {
         int[] nums = new int[]{2,7,4,9,10,223,111,23,3,39};
-        Selections(nums);
+        SelectionsUp(nums);
+        System.out.println();
+        SelectionsDown(nums);
     }
 }
