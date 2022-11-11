0e0e3db48a161c0e63442b3f6df579291a5cee3d
solbae <sol4854@naver.com>
2022-11-11 10:29:00 +0900

    비트연산 후 이진수 출력

--- DataStructureWithAlgorithm/src/main/java/Algorithm/비밀지도.java
+++ DataStructureWithAlgorithm/src/main/java/Algorithm/비밀지도.java
@@ -1,16 +1,49 @@
 package Algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class 비밀지도 {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
+//    String getBinaryString(int decNum){
+//        String answer = "";
+//        while(decNum > 0){
+//            answer += decNum % 2; //answer는 String 이지만 n%2는 int이다 String + int = String이된다.
+//            decNum /= 2;
+//        }
+//
+//        while(decNum > 0){
+//
+//        }
+//        return answer;
+//    }
+
+    public String solution(int n, int[] arr1, int[] arr2) {
+        String answer = "";
+        String[] sArr1 = new String[n];
+        String[] sArr2 = new String[n];
         for(int i = 0; i < arr1.length;i++){
-            answer[i] = Integer.toBinaryString(arr1[i]);
+            for (int j = 0; j < n; j++) {
+                String binStr1 = Integer.toBinaryString(arr1[i]);
+                String binStr2 = Integer.toBinaryString(arr2[i]);
+                if(binStr1.length() < n){
+                    binStr1 = "0" + binStr1;
+                    sArr1[i] = binStr1;
+                } else {
+                    sArr1[i] = binStr1;
+                }
+                if(binStr2.length() < n){
+                    binStr2 = "0" + binStr2;
+                    sArr2[i] = binStr2;
+                } else {
+                    sArr2[i] = binStr2;
+                }
+            }
         }
-        for (String s : answer) {
-            System.out.println(s);
-        }
+        System.out.println(Arrays.toString(sArr1));
+        System.out.println(Arrays.toString(sArr2));
+
         return answer;
     }
 
