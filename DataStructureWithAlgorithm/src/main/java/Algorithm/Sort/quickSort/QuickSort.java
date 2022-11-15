70092e79fb3b42c9ad9605fed75c29abdc346330
solbae <sol4854@naver.com>
2022-11-15 11:02:45 +0900

    퀵정렬 List와 재귀로 풀이

--- DataStructureWithAlgorithm/src/main/java/Algorithm/Sort/quickSort/QuickSort.java
+++ DataStructureWithAlgorithm/src/main/java/Algorithm/Sort/quickSort/QuickSort.java
@@ -2,33 +2,50 @@ package Algorithm.Sort.quickSort;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class QuickSort {
-    public void solution(int[] arr, int start, int end){
-        int pivot = arr.length / 2;
-
-        List<Integer> leftList = new ArrayList<>();
-        for (int i = 0; i < pivot; i++) leftList.add(arr[i]);
-        List<Integer> rightList = new ArrayList<>();
-        for (int i = pivot; i < arr.length; i++) rightList.add(arr[i]);
-
-        for (Integer integer : leftList) {
-            System.out.printf("%d ,", integer);
-        }
-        System.out.println();
-        for (Integer integer : rightList) {
-            System.out.printf("%d ,", integer);
-        }
+    public List<Integer> merge(List<Integer> left, List<Integer> mid, List<Integer> right){
+//        System.out.println("left = " + left);
+//        System.out.println("right = " + right);
+//        System.out.println("mid = " + mid);
+        List<Integer> answer = new ArrayList<>();
+        answer.addAll(left);
+        answer.addAll(mid);
+        answer.addAll(right);
+//        System.out.println("merge = " + answer);
+        return answer;
     }
+    public List<Integer> sort(List<Integer> arr){
+        if(arr.size() <= 1) return arr; //탈출조건 비어있거나 사이즈가 1인 arr가 들어오면 바로 리턴한다.
 
+        int pivot = arr.get(arr.size() / 2);
+//        System.out.println("pivoit = " + pivot);
 
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> mid = new ArrayList<>();
 
+        for (int i = 0; i < arr.size(); i++){
+            if(arr.get(i) < pivot) left.add(arr.get(i));
+            else if(arr.get(i)> pivot) right.add(arr.get(i));
+            else mid.add(arr.get(i)); //피벗값 넣어주는것
+        }
 
+//        System.out.println("left = " + left);
+//        System.out.println("right = " + right);
+//        System.out.println("mid = " + mid);
+        return merge(sort(left), mid, sort(right));
+    }
 
     public static void main(String[] args) {
         int[] arr = new int[]{20, 18, 5, 19, 5, 25, 40, 50};
+        List<Integer> arrList = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) arrList.add(arr[i]);
+
         QuickSort q = new QuickSort();
-        q.solution(arr, 0 , arr.length - 1);
+        System.out.println("Original"+Arrays.toString(arr));
+        System.out.println("answer = "+ q.sort(arrList));
     }
 }
