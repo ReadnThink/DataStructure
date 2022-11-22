b82941c9189c98f031c650298f0e90bd36fc29bc
solbae <sol4854@naver.com>
2022-11-22 10:26:15 +0900

    List에있는 값 더하기

---
+++ DataStructureWithAlgorithm/src/main/java/Recursive/PrintList.java
@@ -0,0 +1,25 @@
+package Recursive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintList {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        nums.add(7);
+        nums.add(3);
+        nums.add(2);
+        nums.add(9);
+        System.out.println(sum(nums));
+    }
+
+    private static int sum(List<Integer> nums) {
+        if(nums.isEmpty()) return 0;
+        /*
+         * return  sum(nums) + nums.remove(nums.size() - 1);
+         * 이렇게 재귀를 호출하게되면 sum(nums)를 먼저 재귀를 타고들어가기 때문에 무한반복이 돈다.
+        * */
+        return  nums.remove(nums.size() - 1) + sum(nums) ;
+    }
+
+}
