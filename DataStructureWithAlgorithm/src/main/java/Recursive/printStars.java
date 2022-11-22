88e2422385ad6bef4435f68d0b3e512e7aa5ca93
solbae <sol4854@naver.com>
2022-11-22 09:29:58 +0900

    별출력

---
+++ DataStructureWithAlgorithm/src/main/java/Recursive/printStars.java
@@ -0,0 +1,18 @@
+package Recursive;
+
+import java.util.Scanner;
+
+public class printStars {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        f(n);
+    }
+
+    private static void f(int n) {
+        if(n <= 0) return;
+        f(n - 1);
+        System.out.print("*");
+    }
+}
+
