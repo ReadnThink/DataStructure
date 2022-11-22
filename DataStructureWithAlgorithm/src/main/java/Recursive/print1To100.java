4f46b3d03e3eb18353af8414756519f7627c7556
solbae <sol4854@naver.com>
2022-11-22 09:19:24 +0900

    print 1 to 100 풀이

---
+++ DataStructureWithAlgorithm/src/main/java/Recursive/print1To100.java
@@ -0,0 +1,17 @@
+package Recursive;
+
+public class print1To100 {
+
+    public static void main(String[] args) {
+        f(0);
+    }
+
+    private static int f(int k) {
+        if(k == 100) return 0;
+        else {
+            System.out.println(k + 1);
+            f(k + 1);
+        }
+        return 0;
+    }
+}
