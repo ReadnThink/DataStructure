7135f4f8e778e9dd33a10dd45695a147eefd53ac
solbae <sol4854@naver.com>
2022-11-25 09:46:06 +0900

    최대공약수 재귀 없이 풀이

--- DataStructureWithAlgorithm/src/main/java/Recursive/CodeUp1857.java
+++ DataStructureWithAlgorithm/src/main/java/Recursive/CodeUp1857.java
@@ -4,27 +4,37 @@ import java.util.Scanner;
 
 public class CodeUp1857 {
     /*
-    * 서로 다른 n개 중에서 r개 순서없이 고르기
-    * */
-
+     * 서로 다른 n개 중에서 r개 순서없이 고르기
+     * */
     static int a;
     static int b;
-    static int cnt = 0;
+    static int[] arr;
+    static boolean[] isUsed;
 
-    public static void main(String[] args){
+    static void func(int k) {
+        if (k == b) {
+            for (int i = 0; i < b; i++)
+                System.out.printf("%d ", arr[i]);
+            System.out.println();
+        }
+        else {
+            for (int i = 1; i <= a; i++) {
+                if (!isUsed[i]) {
+                    arr[k] = i;
+                    isUsed[i] = true;
+                    func(k + 1);
+                    isUsed[i] = false;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         a = sc.nextInt();
         b = sc.nextInt();
-
-        f(1);
-        System.out.println(cnt);
-    }
-
-    private static void f(int k) {
-        if(k == b || k == a) {
-           cnt++;
-           return;
-        }
-        f(k + 1);
+        arr = new int[a + 2];
+        isUsed = new boolean[a + 2];
+        func(0);
     }
 }
--- DataStructureWithAlgorithm/src/main/java/Recursive/CodeUp2623.java
+++ DataStructureWithAlgorithm/src/main/java/Recursive/CodeUp2623.java
@@ -11,12 +11,28 @@ public class CodeUp2623 {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
+        //재귀
         System.out.println(f(a, b));
+
+        // 재귀없이
+        System.out.println(solution(a, b));
     }
 
     private static int f(int a, int b) {
         if(a == b) return a;
 
         return a > b ? f(a - b, b) : f(a, b - a);
+    }
+
+    //재귀 없이 풀이
+    private static int solution(int a, int b) {
+        int max = a > b ? a : b;
+        int min = a > b ? b : a;
+
+        while (max > min) {
+            max -= min;
+        }
+        if(max == min) return max;
+        else return -1;
     }
 }
