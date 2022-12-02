433d52dbd22dbea02f48085cc55ad0c65b3fe2ca
solbae <sol4854@naver.com>
2022-12-02 09:30:28 +0900

    재귀로 소인수분해 풀이

---
+++ DataStructureWithAlgorithm/src/main/java/Algorithm/소인수분해/CodeUp2203.java
@@ -0,0 +1,15 @@
+package Algorithm.소인수분해;
+
+import java.util.Scanner;
+
+public class CodeUp2203 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        solution(n);
+    }
+
+    private static void solution(int n) {
+    }
+}
---
+++ DataStructureWithAlgorithm/src/main/java/Algorithm/소인수분해/CodeUp4037.java
@@ -0,0 +1,25 @@
+package Algorithm.소인수분해;
+
+public class CodeUp4037 {
+/*
+    소인수분해
+    35 = 5 × 7
+    72 = 2 × 2 × 2 × 3 × 3
+    99,380 = 2 × 2 × 5 × 4,969
+*/
+
+    //재귀로 풀이
+    public static void findPrimeFactor(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.printf("%d ",i);
+                findPrimeFactor(n / i);
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        findPrimeFactor(72);
+    }
+}
