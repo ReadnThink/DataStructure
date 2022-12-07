b6f05ab24f6a7e2d7cddcc6b0c7d02385f563fc1
solbae <sol4854@naver.com>
2022-12-07 10:23:37 +0900

    사이즈가 1이 될때 에러 해결

--- DataStructureWithAlgorithm/src/main/java/heap/TheHotter.java
+++ DataStructureWithAlgorithm/src/main/java/heap/TheHotter.java
@@ -4,25 +4,27 @@ import java.util.PriorityQueue;
 
 public class TheHotter {
 
-    public static int solution(int[] arr, int k) {
-        int cnt = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for (int i = 0; i < arr.length; i++) queue.add(arr[i]);
+    public static int solution(int[] arr, long k) {
+        PriorityQueue<Long> queue = new PriorityQueue<>();
+        for (int i = 0; i < arr.length; i++) queue.add((long) arr[i]);
 
-        while (true) {
-            int small = queue.poll();
-            if(small >= k) return cnt;
-            else {
-                cnt++;
-                int sec = queue.poll();
-                small = small + (sec * 2);
-                queue.add(small);
-            }
+        int cnt = 0;
+        while (queue.peek() < k) {
+            if(cnt >= 1 && queue.peek() == 0) return -1;
+            if(queue.size() <= 1) return -1;
+            cnt++;
+            long small = queue.poll();
+            long sec = queue.poll();
+            if(small < 0 && sec < 0) return -1;
+            small = small + (sec * 2);
+            queue.add(small);
         }
+        return cnt;
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 3, 9, 10};
+        int[] arr = new int[]{1, 0, 2};
+        System.out.println(arr.length);
         System.out.println(solution(arr, 7));
     }
 
