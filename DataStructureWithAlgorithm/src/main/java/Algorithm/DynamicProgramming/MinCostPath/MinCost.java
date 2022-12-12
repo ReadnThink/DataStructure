6612476de3daadc7052446ebdac0e64b00532a0f
solbae <sol4854@naver.com>
2022-12-12 09:58:03 +0900

    첫번째 칸 구한느 로직

--- DataStructureWithAlgorithm/src/main/java/Algorithm/DynamicProgramming/MinCostPath/MinCost.java
+++ DataStructureWithAlgorithm/src/main/java/Algorithm/DynamicProgramming/MinCostPath/MinCost.java
@@ -2,15 +2,25 @@ package Algorithm.DynamicProgramming.MinCostPath;
 
 import Algorithm.소수찾기.에라토스테네스의체Array;
 
+import java.util.Arrays;
+
 public class MinCost {
 
     public int getMinCost(int[][] arr) {
         int cnt = 0;
         int[][] dp = new int[arr.length][arr[0].length];
 
-        dp[0][0] = arr[0][0];
-
-
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if(i == 0 && j == 0)
+                    dp[0][0] = arr[0][0];
+                else if (i == 0)
+                    dp[i][j] = dp[i][j - 1] + arr[i][j];
+            }
+        }
+        for (int i = 0; i < dp.length; i++) {
+            System.out.println(Arrays.toString(dp[i]));
+        }
         return cnt;
     }
     public static void main(String[] args) {
